2143eaa0297d4d1d3c3c447b2451f7335c072867
Arun Saju <arunsajukurian5@gmail.com>
2021-01-18 16:18:44 -0500

    Multitheaded delete

--- dx_delete_vdb.py
+++ dx_delete_vdb.py
@@ -9,25 +9,25 @@
 # this doc to also define our ARGUMENTS for the script.
 """Refresh a vdb
 Usage:
-  dx_delete_vdb.py --vdb <name>
-  [--engine <identifier>]
+  dx_delete_vdb.py --vdb <vdb>
+  [--d <enginename>]
   [--group_name <groupname>][--force][--debug]
   [--poll <n>] [--single_thread <bool>]
   [--config <path_to_file>] [--logdir <path_to_file>]
   dx_delete_vdb.py -h | --help | -v | --version
-  dx_delete_vdb.py -h | --help | -v | --version
+
 Delete a Delphix VDB
 Examples:
-  dx_delete_vdb.py --vdb "aseTest"
-  dx_delete_vdb.py --all_vdbs --group_name "Analytics" --all
+  dx_delete_vdb.py --vdbname "aseTest"
+  dx_delete_vdb.py --vdbname "all" --group_name "Analytics" --engine all
 Options:
-  --vdb <name>              Name of the object you are refreshing.
-  --all_vdbs                Refresh all VDBs that meet the filter criteria.
+  --vdb <vdb>              Name of the VDB to delete.
   --group_name <name>       Name of the group to execute against.
-  --single_thread           Run as a single thread. False if running multiple
-                            threads.
-                            [default: False]
-  --engine <name>           Alt Identifier of Delphix DDP in dxtools.conf.
+  --single_thread <boolean> Run as a single thread. Use True if there are
+                            multiple engines and the operation needs to run
+                            in parallel.
+                            [default: True]
+  --d <name>                Identifier for Delphix engine in dxtools.conf.
                             [default: default]
   --debug                   Enable debug logging
   --parallel <n>            Limit number of jobs to maxjob
@@ -53,7 +53,6 @@ from delphixpy.v1_10_2.web import database
 from delphixpy.v1_10_2.web import vo
 from lib import dlpx_exceptions
 from lib import dx_logging
-from lib import dx_timeflow
 from lib import get_references
 from lib import get_session
 from lib import run_job
@@ -61,9 +60,8 @@ from lib.run_async import run_async
 
 VERSION = "v.0.3.001"
 
-
-def delete_vdb(dlpx_obj, vdb_name, force_delete):
-    dx_logging.print_info(f"Commence Delete")
+def delete_vdb(dlpx_obj, vdb_name,force_delete):
+    dx_logging.print_info(f'Commence Delete')
     container_obj = get_references.find_obj_by_name(
         dlpx_obj.server_session, database, vdb_name
     )
@@ -75,19 +73,19 @@ def delete_vdb(dlpx_obj, vdb_name, force_delete):
         try:
             if source_obj.virtual is not True or source_obj.staging is True:
                 raise dlpx_exceptions.DlpxException(
-                    f"ERROR: {container_obj.name} is not a virtual object\n"
+                    f'ERROR: {container_obj.name} is not a virtual object'
                 )
             else:
                 dx_logging.print_info(
-                    f"INFO: Deleting {container_obj.name} on engine {dlpx_obj.server_session.address}\n"
+                    f'INFO: Deleting {container_obj.name} on engine {dlpx_obj.server_session.address}'
                 )
                 delete_params = None
-                if force_delete and str(container_obj.reference).startswith("MSSQL"):
+                if force_delete and str(container_obj.reference).startswith('MSSQL'):
                     delete_params = vo.DeleteParameters()
                     delete_params.force = True
                 try:
                     database.delete(
-                        dlpx_obj.server_session, container_obj.reference, delete_params
+                        dlpx_obj.server_session, container_obj.reference,delete_params
                     )
                     dlpx_obj.jobs[
                         dlpx_obj.server_session.address
@@ -95,24 +93,24 @@ def delete_vdb(dlpx_obj, vdb_name, force_delete):
                 except (
                     dlpx_exceptions.DlpxException,
                     exceptions.RequestError,
-                    exceptions.HttpError,
+                    exceptions.HttpError
                 ) as err:
-                    raise dlpx_exceptions.DlpxException(f"{err}")
+                    raise dlpx_exceptions.DlpxException( f'{err}')
         # This exception is raised if refreshing a vFiles VDB since
         # AppDataContainer does not have virtual, staging or enabled attributes
         except AttributeError as err:
             dx_logging.print_exception(
-                f"ERROR: Deleting {container_obj.name} on engine {dlpx_obj.server_session.address}\n"
+                f'ERROR: Deleting {container_obj.name} on engine {dlpx_obj.server_session.address}'
             )
-            dx_logging.print_exception(f"AttributeError:{err.message}\n")
+            dx_logging.print_exception(f'AttributeError:{err}')
         except dlpx_exceptions.DlpxException as err:
-            dx_logging.print_exception(f"DlpxException:{err}")
+            dx_logging.print_exception(f'DlpxException:{err}')
         except Exception as err:
-            dx_logging.print_exception(f"Exception:\n{err}")
-        dx_logging.print_info(f"Delete process complete")
+            dx_logging.print_exception(f'Exception:\n{err}')
+        dx_logging.print_info(
+            f'Delete operation has been initiated for * {vdb_name} * on engine * {dlpx_obj.server_session.address} * ')
         # return the last job id.
         return dlpx_obj.server_session.last_job
-
 
 @run_async
 def main_workflow(engine, dlpx_obj, single_thread):
@@ -128,34 +126,32 @@ def main_workflow(engine, dlpx_obj, single_thread):
     :param single_thread: True - run single threaded, False - run multi-thread
     :type single_thread: bool
     """
-    # TODO: Implement multi-threading.
     try:
         # Setup the connection to the Delphix DDP
-        dx_logging.print_info(f"Executing main_workflow")
+        dx_logging.print_info(f'Executing main_workflow')
         dlpx_obj.dlpx_session(
-            engine["ip_address"], engine["username"], engine["password"]
+            engine['ip_address'], engine['username'], engine['password']
         )
     except dlpx_exceptions.DlpxException as err:
         dx_logging.print_exception(
-            f"ERROR: dx_refresh_vdb encountered an error authenticating to "
+            f'ERROR: dx_refresh_vdb encountered an error authenticating to '
             f'{engine["hostname"]} {ARGUMENTS["--target"]}:\n{err}\n'
         )
     try:
         with dlpx_obj.job_mode(single_thread):
-            job_id = delete_vdb(dlpx_obj, ARGUMENTS["--vdb"], ARGUMENTS["--force"])
+            job_id = delete_vdb( dlpx_obj, ARGUMENTS['--vdb'],ARGUMENTS['--force'])
             # locking threads
-            run_job.find_job_state_by_jobid(engine, dlpx_obj, job_id)
+            #run_job.find_job_state_by_jobid(engine, dlpx_obj,job_id)
     except (
         dlpx_exceptions.DlpxException,
         dlpx_exceptions.DlpxObjectNotFound,
         exceptions.RequestError,
         exceptions.JobError,
-        exceptions.HttpError,
+        exceptions.HttpError
     ) as err:
         dx_logging.print_exception(
-            f'Error in dx_delete_vdb on Delpihx Engine: {engine["ip_address"]}\n{err}'
+            f'Error in dx_delete_vdb on Delpihx Engine: * {engine["ip_address"]} * : {err}'
         )
-
 
 def main():
     """
@@ -164,21 +160,26 @@ def main():
     time_start = time.time()
     try:
         dx_session_obj = get_session.GetSession()
-        dx_logging.logging_est(ARGUMENTS["--logdir"])
-        config_file_path = ARGUMENTS["--config"]
-        single_thread = ARGUMENTS["--single_thread"]
-        engine = ARGUMENTS["--engine"]
+        dx_logging.logging_est(ARGUMENTS['--logdir'])
+        config_file_path = ARGUMENTS['--config']
+        single_thread = ARGUMENTS['--single_thread']
+        engine = ARGUMENTS["--d"]
+        #vdbname = ARGUMENTS["--vdb"]
         dx_session_obj.get_config(config_file_path)
         # This is the function that will handle processing main_workflow for all the servers.
-        for each in run_job.run_job(
+        t = run_job.run_job_mt(
             main_workflow, dx_session_obj, engine, single_thread
-        ):
+        )
+        dx_logging.print_info(
+            f'delete operation(s) are in progress. Process will terminate once all operations are complete.'
+        )
+        for each in t:
             # join them back together so that we wait for all threads to complete
             each.join()
             # waits for threads to finish.
         elapsed_minutes = run_job.time_elapsed(time_start)
         dx_logging.print_info(
-            f"delete operation took {elapsed_minutes} minutes to complete."
+            f'delete operation took {elapsed_minutes} minutes to complete.'
         )
     # Here we handle what we do when the unexpected happens
     except SystemExit as err:
@@ -189,14 +190,14 @@ def main():
         # We use this exception handler when an error occurs in a function
         # call.
         dx_logging.print_exception(
-            f"ERROR: Please check the ERROR message below:\n {err.error}"
+            f'ERROR: Please check the ERROR message below:\n {err.error}'
         )
         sys.exit(2)
 
     except exceptions.HttpError as err:
         # We use this exception handler when our connection to Delphix fails
         dx_logging.print_exception(
-            f"ERROR: Connection failed to the Delphix DDP. Please check the ERROR message below:\n{err.status}"
+            f'ERROR: Connection failed to the Delphix DDP. Please check the ERROR message below:\n{err.status}'
         )
         sys.exit(2)
 
@@ -205,18 +206,17 @@ def main():
         # have actionable data
         elapsed_minutes = run_job.time_elapsed(time_start)
         dx_logging.print_exception(
-            f"A job failed in the Delphix Engine:\n{err.job}."
-            f"{basename(__file__)} took {elapsed_minutes} minutes to get this far"
+            f'A job failed in the Delphix Engine:\n{err.job}.'
+            f'{basename(__file__)} took {elapsed_minutes} minutes to get this far'
         )
         sys.exit(3)
 
     except KeyboardInterrupt:
         # We use this exception handler to gracefully handle ctrl+c exits
-        dx_logging.print_debug("You sent a CTRL+C to interrupt the process")
+        dx_logging.print_debug('You sent a CTRL+C to interrupt the process')
         elapsed_minutes = run_job.time_elapsed(time_start)
         dx_logging.print_info(
-            f"{basename(__file__)} took {elapsed_minutes} " f"minutes to get this far."
-        )
+            f'{basename(__file__)} took {elapsed_minutes} minutes to get this far.')
 
 
 if __name__ == "__main__":
--- lib/run_job.py
+++ lib/run_job.py
@@ -5,11 +5,12 @@ import time
 
 from delphixpy.v1_10_2 import exceptions
 from delphixpy.v1_10_2.web import job
-from lib import dlpx_exceptions
+
 from lib import dx_logging
+from lib import dlpx_exceptions
 
 
-def run_job(main_func, dx_obj, engine="default", single_thread=True):
+def run_job(main_func, dx_obj, engine='default', single_thread=True):
     """
     This method runs the main_func asynchronously against all the
     servers specified
@@ -25,42 +26,98 @@ def run_job(main_func, dx_obj, engine="default", single_thread=True):
     :type single_thread: bool
     """
     # If "all" argument was given, run against every engine in config_file
-    if engine == "all":
-        dx_logging.print_info(f"Executing against all Delphix DDPs")
+    if engine == 'all':
+        dx_logging.print_info(f'Executing against all Delphix DDPs')
         try:
             for delphix_ddp in dx_obj.dlpx_ddps:
                 yield main_func(dx_obj.dlpx_ddps[delphix_ddp], dx_obj, single_thread)
         except dlpx_exceptions.DlpxException as err:
-            dx_logging.print_exception(f"Error encountered in run_job():\n{err}")
-    elif engine == "default":
+            dx_logging.print_exception(f'Error encountered in run_job():\n{err}')
+    elif engine == 'default':
         try:
             for delphix_ddp in dx_obj.dlpx_ddps.keys():
-                if dx_obj.dlpx_ddps[delphix_ddp]["default"] == "True":
+                if dx_obj.dlpx_ddps[delphix_ddp]['default'] == 'True':
                     dx_obj_default = dx_obj
                     dx_obj_default.dlpx_ddps = {
-                        delphix_ddp: dx_obj.dlpx_ddps[delphix_ddp]
-                    }
-                    dx_logging.print_info(f"Executing against default DDP")
-                    yield main_func(
-                        dx_obj.dlpx_ddps[delphix_ddp], dx_obj, single_thread
-                    )
+                        delphix_ddp: dx_obj.dlpx_ddps[delphix_ddp]}
+                    dx_logging.print_info(f'Executing against default DDP')
+                    yield main_func(dx_obj.dlpx_ddps[delphix_ddp], dx_obj, single_thread)
                 break
         except TypeError as err:
-            raise dlpx_exceptions.DlpxException(f"Error in run_job: {err}")
+            raise dlpx_exceptions.DlpxException(f'Error in run_job: {err}')
     else:
         # Test to see if the engine exists in config_file
         try:
             yield main_func(dx_obj.dlpx_ddps[engine], dx_obj, single_thread)
-            dx_logging.print_info(
-                f"Executing against Delphix DDP: " f"{dx_obj.dlpx_ddps[engine]}"
-            )
+            dx_logging.print_info(f'Executing against Delphix DDP: '
+                                  f'{dx_obj.dlpx_ddps[engine]}')
         except (exceptions.RequestError, KeyError):
             raise dlpx_exceptions.DlpxException(
-                f"\nERROR: Delphix DDP {engine} cannot be found. Please "
-                f"check your value and try again."
-            )
+                f'\nERROR: Delphix DDP {engine} cannot be found. Please '
+                f'check your value and try again.')
     if engine is None:
-        raise dlpx_exceptions.DlpxException(f"ERROR: No default Delphix " f"DDP found.")
+        raise dlpx_exceptions.DlpxException(f'ERROR: No default Delphix '
+                                            f'DDP found.')
+
+
+def run_job_mt(main_func, dx_obj, engine='default', single_thread=True):
+    """
+    This method runs the main_func asynchronously against all the
+    delphix engines specified
+    :param main_func: function to run against the DDP(s).
+     In these examples, it's main_workflow().
+    :type main_func: function
+    :param dx_obj: Delphix session object from config
+    :type dx_obj: lib.get_session.GetSession object
+    :param engine: name of an engine, all or None
+    :type engine: str
+    :param single_thread: Run as single thread (True) or
+                    multiple threads (False)
+    :type single_thread: bool
+    """
+    threads = []
+    # if engine ="all", run against every engine in config_file
+    if engine == 'all':
+        dx_logging.print_info(f'Executing against all Delphix DDPs')
+        try:
+            for delphix_ddp in dx_obj.dlpx_ddps:
+                t = main_func(dx_obj.dlpx_ddps[delphix_ddp], dx_obj, single_thread)
+                threads.append(t)
+                # TODO: Revisit threading logic
+                # This sleep has been tactically added to prevent errors in the parallel
+                # processing of operations across multiple engines
+                time.sleep(5)
+        except dlpx_exceptions.DlpxException as err:
+            dx_logging.print_exception(f'Error encountered in run_job():\n{err}')
+    elif engine == 'default':
+        try:
+            for delphix_ddp in dx_obj.dlpx_ddps.keys():
+                if dx_obj.dlpx_ddps[delphix_ddp]['default'] == 'True':
+                    dx_obj_default = dx_obj
+                    dx_obj_default.dlpx_ddps = {
+                        delphix_ddp: dx_obj.dlpx_ddps[delphix_ddp]}
+                    dx_logging.print_info(f'Executing against default DDP')
+                    t=main_func(dx_obj.dlpx_ddps[delphix_ddp], dx_obj, single_thread)
+                    threads.append(t)
+                break
+        except TypeError as err:
+            raise dlpx_exceptions.DlpxException(f'Error in run_job: {err}')
+    else:
+        # Test to see if the engine exists in config_file
+        try:
+            t = main_func(dx_obj.dlpx_ddps[engine], dx_obj, single_thread)
+            threads.append(t)
+            dx_logging.print_info(f'Executing against Delphix DDP: '
+                                  f'{dx_obj.dlpx_ddps[engine]}')
+        except (exceptions.RequestError, KeyError):
+            raise dlpx_exceptions.DlpxException(
+                f'\nERROR: Delphix DDP {engine} cannot be found. Please '
+                f'check your value and try again.')
+    if engine is None:
+        raise dlpx_exceptions.DlpxException(f'ERROR: No default Delphix '
+                                            f'DDP found.')
+    return threads
+
 
 
 def find_job_state(engine, dx_obj, poll=10):
@@ -78,14 +135,12 @@ def find_job_state(engine, dx_obj, poll=10):
     for j in dx_obj.jobs.keys():
         job_obj = job.get(dx_obj.server_session, dx_obj.jobs[j])
         dx_logging.print_debug(job_obj)
-        dx_logging.print_info(
-            f'{engine["ddp_identifier"]}: Running job: {job_obj.job_state}'
-        )
-        if job_obj.job_state in ["CANCELED", "COMPLETED", "FAILED"]:
+        dx_logging.print_info( f'{engine["ddp_identifier"]}: Running job: {job_obj.job_state}')
+        if job_obj.job_state in ['CANCELED', 'COMPLETED', 'FAILED']:
             # If the job is in a non-running state, remove it
             # from the running jobs list.
             del dx_obj.jobs[j]
-        elif job_obj.job_state in "RUNNING":
+        elif job_obj.job_state in 'RUNNING':
             # If the job is in a running state, increment the
             # running job count.
             i += 1
@@ -96,7 +151,7 @@ def find_job_state(engine, dx_obj, poll=10):
             time.sleep(poll)
 
 
-def find_job_state_by_jobid(engine, dx_obj, job_id, poll=20):
+def find_job_state_by_jobid(engine, dx_obj,job_id, poll=20):
     """
     Retrieves running job state
     :param engine: Dictionary containing info on the DDP (IP, username, etc.)
@@ -110,17 +165,14 @@ def find_job_state_by_jobid(engine, dx_obj, job_id, poll=20):
     # get the job object
     job_obj = job.get(dx_obj.server_session, job_id)
     dx_logging.print_debug(job_obj)
-    dx_logging.print_info(f" Polling for : {job_id} to finish")
-    while job_obj.job_state == "RUNNING":
+    dx_logging.print_info(f' Polling for : {job_id} to finish')
+    while job_obj.job_state == 'RUNNING':
         time.sleep(poll)
         job_obj = job.get(dx_obj.server_session, job_id)
-    dx_logging.print_info(f"Job: {job_id} completed with status: {job_obj.job_state}")
-    if job_obj.job_state == "FAILED":
-        raise dlpx_exceptions.DlpxException(
-            "Job: {job_id} failed. Please check the error and retry."
-        )
+    dx_logging.print_info(f'Job: {job_id} completed with status: {job_obj.job_state}')
+    if job_obj.job_state =='FAILED':
+        raise dlpx_exceptions.DlpxException('Job: {job_id} failed. Please check the error and retry.')
     # TODO: Pass the error message back to calling function.
-
 
 def time_elapsed(time_start):
     """
@@ -130,4 +182,4 @@ def time_elapsed(time_start):
     :param time_start: start time of the script.
     :type time_start: float
     """
-    return round((time.time() - time_start) / 60, +1)
+    return round((time.time() - time_start)/60, +1)
